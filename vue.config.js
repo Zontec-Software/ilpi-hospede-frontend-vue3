b7a06feb3d782f8825de09d1dc10256c0e3f60bd
Lucas Lima Gonçalves <150838757+LucasLg1@users.noreply.github.com>
2025-01-14 13:57:46 -0300

    att

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/<nome-do-repo>/' : '/',
   transpileDependencies: true
 })
